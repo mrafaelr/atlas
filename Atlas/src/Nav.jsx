19c032a06d335f7d9ef4c99e90f38394595d6c01
Rafael Monteiro Rodrigues <rafaelmontro1996@gmail.com>
2023-06-20 13:37:18 -0300

    start nav

---
+++ Atlas/src/Nav.jsx
@@ -0,0 +1,18 @@
+
+export default function Nav() {
+    
+    return (
+      <header>
+        <div id="logo">
+          ATLAS
+        </div>
+        <nav>
+          <ul>
+            <li><a href="">1. Dados pessoais</a></li>
+            <li><a href="">2. Montagem da dieta</a></li>
+            <li><a href="">3. Download</a></li>
+          </ul>
+        </nav>
+      </header>
+    );
+}
