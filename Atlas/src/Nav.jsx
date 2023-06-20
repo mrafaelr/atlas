30cc757566fb613c4294f41574a0c7c58f30c769
Rafael Monteiro Rodrigues <rafaelmontro1996@gmail.com>
2023-06-20 15:33:15 -0300

    style nav buttons

--- Atlas/src/Nav.jsx
+++ Atlas/src/Nav.jsx
@@ -9,9 +9,9 @@ export default function Nav() {
         </div>
         <nav>
           <ul>
-            <li><a href="">1. Dados pessoais</a></li>
-            <li><a href="">2. Montagem da dieta</a></li>
-            <li><a href="">3. Download</a></li>
+            <li><a href="">1.Dados pessoais</a></li>
+            <li><a href="">2.Montagem da dieta</a></li>
+            <li><a href="">3.Download</a></li>
           </ul>
         </nav>
       </header>
